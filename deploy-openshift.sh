c8dc7abf42633e58841d1ba2c9421470e70b9c39
raphac-mar <159266229+raphac-mar@users.noreply.github.com>
2024-07-29 17:18:00 -0300

    Add files via upload

---
+++ deploy-openshift.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "############################################################"
+echo "Namespace"
+echo "############################################################"
+
+NAMESPACE_SONARQUBE=$1
+if [ "$NAMESPACE_SONARQUBE" == "" ]; then
+  NAMESPACE_SONARQUBE='sonarqube'
+fi
+echo "[INFO] Namespace: $NAMESPACE_SONARQUBE"
+
+oc project $NAMESPACE_SONARQUBE &>/dev/null
+
+if [ $? -eq 1 ]; then
+  echo "[INFO] Create $NAMESPACE_SONARQUBE namespace first"
+  oc new-project $NAMESPACE_SONARQUBE
+fi
+
+echo "############################################################"
+echo "Build image"
+echo "############################################################"
+oc apply -f k8s/git-auth-repo-sonarqube.yaml
+oc new-build . --name=sonarqube --strategy=docker -l "app=sonarqube" --to="image-registry.openshift-image-registry.svc:5000/sonarqube/sonarqube:10.6.0.92116" --source-secret git-auth-repo-sonarqube
+sleep 3
+oc logs -f bc/sonarqube
+echo ""
+
+echo "############################################################"
+echo "Install postgresql"
+echo "############################################################"
+oc apply -k pg/base
+echo ""
+
+echo "############################################################"
+echo "Install sonarqube"
+echo "############################################################"
+oc apply -k sonarqube/base
+echo ""
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Sonarqube
+
+- Versão: 10.6.0.92116
+
+## Referencia dockerfile
+
+- https://github.com/SonarSource/docker-sonarqube/blob/master/10/community/Dockerfile
+
+## Referencia helm
+
+- https://github.com/SonarSource/helm-chart-sonarqube/tree/master/charts/sonarqube
